7bb6dbb560e456546bbcd74fd2de82c4a15e8a6d
Clement Debroize <clement.debroize@gmail.com>
2017-07-01 14:41:31 +0200

    Lancement de l'operation cobra

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Home from './components/home/home.js';
 
 class App extends Component {
     render(){
         return (
-            <h1>Hello </h1>
+            <Home/>
         )
     }
 }
